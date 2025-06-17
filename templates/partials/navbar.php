a15cfc58765fa274c237968ba7d2a3679f720800
Greg Sarjeant <1686767+gsarjeant@users.noreply.github.com>
2025-06-17 18:05:14 -0400

    Convert dropdown menus to details/summary tags

--- templates/partials/navbar.php
+++ templates/partials/navbar.php
@@ -2,24 +2,24 @@
 <?php /* https://www.w3schools.com/howto/howto_css_dropdown.asp */ ?>
         <div class="navbar">
             <a href="<?= $config->basePath ?>">home</a>
-            <div class="dropdown">
-                <button class="dropbtn">feeds</button>
+            <details class="dropdown">
+                <summary class="dropbtn">feeds</summary>
                 <div class="dropdown-content">
                     <a href="<?= $config->basePath ?>feed/rss">rss</a>
                     <a href="<?= $config->basePath ?>feed/atom">atom</a>
                 </div>
-            </div>
+            </details>
 <?php if (!Session::isLoggedIn()): ?>
             <a href="<?= $config->basePath ?>login">login</a>
 <?php else: ?>
-            <div class="dropdown">
-                <button class="dropbtn">admin</button>
+            <details class="dropdown">
+                <summary class="dropbtn">admin</summary>
                 <div class="dropdown-content">
                     <a href="<?= $config->basePath ?>admin">settings</a>
                     <a href="<?= $config->basePath ?>admin/css">css</a>
                     <a href="<?= $config->basePath ?>admin/emoji">emoji</a>
                 </div>
-            </div>
+            </details>
             <a href="<?= $config->basePath ?>logout">logout</a>
 <?php endif; ?>
         </div>

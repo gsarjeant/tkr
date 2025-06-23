b8e77a492f281c0922fe728537348c32138e2bbd
Greg Sarjeant <1686767+gsarjeant@users.noreply.github.com>
2025-06-23 10:16:39 -0400

    Fix admin accessibility errors.

--- templates/partials/admin.php
+++ templates/partials/admin.php
@@ -2,7 +2,7 @@
 <?php /** @var UserModel $user */ ?>
 <?php /** @var isSetup bool */ ?>
         <h1><?php if ($isSetup): ?>Setup<?php else: ?>Admin<?php endif; ?></h1>
-        <div>
+        <main>
             <form
                 action="<?php echo $config->basePath . ($isSetup ? 'setup' : 'admin') ?>"
                 method="post">
@@ -10,22 +10,26 @@
                 <fieldset>
                     <legend>User settings</legend>
                     <div class="fieldset-items">
-                        <label>Username <span class=required>*</span></label>
+                        <label for="username">Username <span class=required>*</span></label>
                         <input type="text"
+                            id="username"
                             name="username"
                             value="<?= Util::escape_html($user->username) ?>"
                             required>
-                        <label>Display name <span class=required>*</span></label>
+                        <label for="display_name">Display name <span class=required>*</span></label>
                         <input type="text"
+                               id="display_name"
                                name="display_name"
                                value="<?= Util::escape_html($user->displayName) ?>"
                                required>
-                        <label>About </label>
+                        <label for="about">About </label>
                         <input type="text"
+                            id="about"
                             name="about"
                             value="<?= Util::escape_html($user->about) ?>">
-                        <label>Website </label>
+                        <label for="website">Website </label>
                         <input type="text"
+                            id="website"
                             name="website"
                             value="<?= Util::escape_html($user->website) ?>">
                     </div>
@@ -33,37 +37,42 @@
                 <fieldset>
                     <legend>Site settings</legend>
                     <div class="fieldset-items">
-                        <label>Title <span class=required>*</span></label>
+                        <label for="site_title">Title <span class=required>*</span></label>
                         <input type="text"
+                            id="site_title"
                             name="site_title"
                             value="<?= Util::escape_html($config->siteTitle) ?>"
                             required>
-                        <label>Description <span class=required>*</span></label>
+                        <label for="site_description">Description <span class=required>*</span></label>
                         <input type="text"
+                            id="site_description"
                             name="site_description"
                             value="<?= Util::escape_html($config->siteDescription) ?>">
-                        <label>Base URL <span class=required>*</span></label>
+                        <label for="base_url">Base URL <span class=required>*</span></label>
                         <input type="text"
+                            id="base_url"
                             name="base_url"
                             value="<?= Util::escape_html($config->baseUrl) ?>"
                             required>
-                        <label>Base path <span class=required>*</span></label>
+                        <label for="base_path">Base path <span class=required>*</span></label>
                         <input type="text"
+                            id="base_path"
                             name="base_path"
                             value="<?= Util::escape_html($config->basePath) ?>"
                             required>
-                        <label>Items per page (max 50) <span class=required>*</span></label>
+                        <label for="items_per_page">Items per page (max 50) <span class=required>*</span></label>
                         <input type="number"
+                            id="items_per_page"
                             name="items_per_page"
                             value="<?= $config->itemsPerPage ?>" min="1" max="50"
                             required>
-                        <label>Strict accessibility</label>
+                        <label for="strict_accessibility">Strict accessibility</label>
                         <input type="checkbox"
                                id="strict_accessibility"
                                name="strict_accessibility"
                                value="1"
                                <?php if ($config->strictAccessibility): ?> checked <?php endif; ?>>
-                        <label>Show tick mood</label>
+                        <label for="show_tick_mood">Show tick mood</label>
                         <input type="checkbox"
                                id="show_tick_mood"
                                name="show_tick_mood"
@@ -74,17 +83,19 @@
                 <fieldset>
                     <legend>Change password</legend>
                     <div class="fieldset-items">
-                        <label>New password
+                        <label for="password">New password
                             <?php if($isSetup): ?><span class=required>*</span><?php endif; ?>
                         </label>
                         <input type="password"
+                               id="password"
                                name="password"
                                <?php if($isSetup): ?>required <?php endif; ?>
                         >
-                        <label>Confirm new password
+                        <label for="confirm_password">Confirm new password
                             <?php if($isSetup): ?><span class=required>*</span><?php endif; ?>
                         </label>
                         <input type="password"
+                               id="confirm_password"
                                name="confirm_password"
                                <?php if($isSetup): ?>required <?php endif; ?>
                         >
@@ -92,4 +103,4 @@
                 </fieldset>
                 <button type="submit" class="submit-btn">Save Settings</button>
             </form>
-        </div>
+        </main>
